3f0681dbb97a0f0927466226148f4d418f7091d3
nhtan <hoangtan250997@gmail.com>
2024-02-29 17:57:17 +0700

    [CHANGE] position number

--- src/main/java/com/example/producer/Config.java
+++ src/main/java/com/example/producer/Config.java
@@ -1,6 +1,7 @@
 package com.example.producer;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,8 @@ import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class Config {
-    private final String topicName ="${topic.name}";
+    @Value("${topic.name}")
+    private String topicName;
     private final KafkaProperties kafkaProperties;
     public Config(KafkaProperties kafkaProperties) {
         this.kafkaProperties = kafkaProperties;
@@ -18,7 +20,7 @@ public class Config {
     public NewTopic topic() {
         return TopicBuilder
                 .name(topicName)
-                .partitions(2)
+                .partitions(1)
                 .replicas(1)
                 .build();
     }
